108c27fca5904db5166678df08b6fd7d43667d42
lucky <nitinkumawat8000@gmail.com>
2024-03-06 00:04:18 +0530

    new changes

--- app.js
+++ app.js
@@ -15,3 +15,21 @@ let show = () => {
   }
 };
 btn.addEventListener("click", show);
+
+document.querySelectorAll(".anime").forEach((el) => {
+  let endValue = el.getAttribute("data-end-value");
+  let startValue = el.getAttribute("data-increment");
+  let durationValue = el.getAttribute("data-duration");
+
+  if (endValue) numberAnimation(el, endValue, startValue, durationValue);
+});
+
+function numberAnimation(el, endValue, incrementor, duration) {
+  anime({
+    targets: el,
+    textContent: endValue,
+    round: incrementor ? 1 / incrementor : 1 / 5,
+    easing: "easeInOutQuad",
+    duration: duration ? duration : 4000,
+  });
+}
